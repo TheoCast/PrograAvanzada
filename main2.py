fff06879cf0f96d97e099c754eb4d7a59cfc9238
TheoCast <159454815+TheoCast@users.noreply.github.com>
2024-02-27 05:15:42 -0600

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,15 @@
+from lib2 import *
+
+inOrderArr=[]
+
+nodo1 = nodo(1)
+nodo2 = nodo(2)
+nodo3 = nodo(3)
+nodo4 = nodo(4)
+nodo5 = nodo(5)
+nodo6 = nodo(6)
+nodo7 = nodo(7)
+
+linkhijo(nodo1, nodo2, nodo3)
+linkhijo(nodo2, nodo4, nodo5)
+linkhijo(nodo3, nodo6, nodo7)
